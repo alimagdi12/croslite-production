9dff74f0b746e81777b9906d1d196ac46edf6f9d
alimagdi12 <alimagdi12367@yahoo.com>
2024-10-31 01:56:06 +0300

    server connection

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+     host: true,
+     port: 3000,
+     https: {
+       key: "./certs/server.key",
+       cert: "./certs/server.crt",
+     },
+  },
+  preview: {
+    host: true,
+    port: 3000,
+    https: {
+      key: './certs/server.key',
+      cert: './certs/server.crt',
+    },
+  },
   plugins: [react()],
 })
